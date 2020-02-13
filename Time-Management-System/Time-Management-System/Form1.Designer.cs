8c92350114372f2ca7f79b2b10d120f564ab5106
HumayunMulla <humayun.mulla@gmail.com>
2020-02-13 17:31:16 -0500

    Minimzed system tray notification.

--- Time-Management-System/Time-Management-System/Form1.Designer.cs
+++ Time-Management-System/Time-Management-System/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Time_Management_System
 {
-    partial class Form1
+    partial class frm_mainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1_attendance = new System.Windows.Forms.TabPage();
+            this.tabPage2_tasklogger = new System.Windows.Forms.TabPage();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1_attendance);
+            this.tabControl1.Controls.Add(this.tabPage2_tasklogger);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(468, 214);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1_attendance
+            // 
+            this.tabPage1_attendance.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1_attendance.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1_attendance.Name = "tabPage1_attendance";
+            this.tabPage1_attendance.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1_attendance.Size = new System.Drawing.Size(460, 182);
+            this.tabPage1_attendance.TabIndex = 0;
+            this.tabPage1_attendance.Text = "Attendance";
+            this.tabPage1_attendance.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2_tasklogger
+            // 
+            this.tabPage2_tasklogger.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2_tasklogger.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2_tasklogger.Name = "tabPage2_tasklogger";
+            this.tabPage2_tasklogger.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2_tasklogger.Size = new System.Drawing.Size(460, 182);
+            this.tabPage2_tasklogger.TabIndex = 1;
+            this.tabPage2_tasklogger.Text = "Task Tracker";
+            this.tabPage2_tasklogger.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "Time Management System";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // frm_mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(468, 214);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frm_mainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Management System";
+            this.Load += new System.EventHandler(this.frm_mainWindow_Load);
+            this.Resize += new System.EventHandler(this.frm_mainWindow_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1_attendance;
+        private System.Windows.Forms.TabPage tabPage2_tasklogger;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- Time-Management-System/Time-Management-System/Form1.cs
+++ Time-Management-System/Time-Management-System/Form1.cs
@@ -10,11 +10,40 @@ using System.Windows.Forms;
 
 namespace Time_Management_System
 {
-    public partial class Form1 : Form
+    public partial class frm_mainWindow : Form
     {
-        public Form1()
+        public frm_mainWindow()
         {
             InitializeComponent();
+        }
+
+        private void frm_mainWindow_Resize(object sender, EventArgs e)
+        {
+            /*  using the Resize Event so that when the form is minized, 
+             *  it will minimize it to the system tray.
+             */
+            notifyIcon.BalloonTipTitle = "Time Management System";
+            notifyIcon.BalloonTipText = "You have successfully minimized the application.";
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(500);
+                this.Hide();
+
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void frm_mainWindow_Load(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = SystemIcons.Information;
         }
     }
 }
